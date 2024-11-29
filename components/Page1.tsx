41c5ec8865f8d767a72a649bf10c7cdaca574d30
Your Name <your.email@example.com>
2024-11-29 23:40:26 +0530

    Update submodule content

---
+++ components/Page1.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Page1 =() => {
+    return (
+<div className='columns-2 bg-gray-950 '>
+<img  className='flex w-full h-full'src="https://www.21-draw.com/wp-content/uploads/2023/11/add-shadows-to-make-your-dog-drawing-more-realistic-e1700119847404.jpg" alt="" />
+<img  className='fle w-full h-full'src="https://www.21-draw.com/wp-content/uploads/2023/11/add-shadows-to-make-your-dog-drawing-more-realistic-e1700119847404.jpg" alt="" />
+</div>
+    )
+}
+export default Page1;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,15 @@
 // pages/index.tsx
 
 import NavBar from '../components/NavBar';
+import Page1 from '../components/Page1';
 
 
 export default function Home() {
   return (
     <div>
       <NavBar />
+
+      <Page1 />
     </div>
   );
 }
